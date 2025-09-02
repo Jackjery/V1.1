6472d5d13cb831c887b11d2f8cebb52c9e72f961
Jackjery <m13289558678@163.com>
2025-09-02 23:28:13 +0800

    Update records.js

--- api/records.js
+++ api/records.js
@@ -44,11 +44,17 @@ export default async function handler(req, res) {
 
         // 添加过滤条件
         if (startDate) {
-            options.startDate = new Date(startDate);
+            const parsedStartDate = new Date(startDate);
+            if (!isNaN(parsedStartDate.getTime())) {
+                options.startDate = parsedStartDate;
+            }
         }
         
         if (endDate) {
-            options.endDate = new Date(endDate);
+            const parsedEndDate = new Date(endDate);
+            if (!isNaN(parsedEndDate.getTime())) {
+                options.endDate = parsedEndDate;
+            }
         }
         
         if (taskResult) {
